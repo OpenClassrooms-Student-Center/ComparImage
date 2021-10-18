d1cdc3cd9fe3aa9ee78a58816253f1308a383d5c
bbloch <b9bloch@yahoo.fr>
2021-10-18 15:08:45 +0200

    Update folder skeleton and move to swift 5

---
+++ starter/ComparImage/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ComparImage
+//
+//  Created by Bertrand Bloc'h 13/10/2021.
+//  Copyright © 2017 OpenClassrooms. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
